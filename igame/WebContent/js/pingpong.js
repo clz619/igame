f3ebdc9ab7b7cb9de1b4dd372baddb426231b226
clz619 <121733807@qq.com>
2013-04-10 17:23:13 +0800

    1.将按键移动更改为循环监听式，使之更流畅可多键盘输入 2.添加游戏边界判读，防止乒乓板移出游戏区域

--- igame/WebContent/js/pingpong.js
+++ igame/WebContent/js/pingpong.js
@@ -5,37 +5,70 @@ var KEY = {
 	S : 83
 };
 
+var pingpong = {};
+pingpong.pressedKeys = [];
+
 $(function() {
-	 $("#paddleB").css("top", "20px");
-	$("#paddleA").css("top", "60px");
+	// 将键盘监听事件更顺畅并且支持同时多个输入
+	// 循环变化按键事件状态
+	pingpong.timer = setInterval(gameloop, 30);
 
 	$(document).keydown(function(e) {
-		switch (e.which) {
-		case KEY.UP:
-			var top = parseInt($("#paddleB").css("top"));
-			$("#paddleB").css("top", top - 5);
-			break;
-		case KEY.DOWN:
-			var top = parseInt($("#paddleB").css("top"));
-			$("#paddleB").css("top", top + 5);
-			break;
-		case KEY.W:
-			var top = parseInt($("#paddleA").css("top"));
-			$("#paddleA").css("top", top - 5);
-			break;
-		case KEY.S:
-			var top = parseInt($("#paddleA").css("top"));
-			$("#paddleA").css("top", top + 5);
-			break;
-		}
+		pingpong.pressedKeys[e.which] = true;
+	});
+	$(document).keyup(function(e) {
+		pingpong.pressedKeys[e.which] = false;
 	});
 
-//	var pingpong = {};
-//	pingpong.pressedKeys = [];
-//	$(document).keydown(function(e) {
-//		pingpong.pressedKeys[e.which] = true;
-//	});
-//	$(document).keyup(function(e) {
-//		pingpong.pressedKeys[e.which] = false;
-//	});
 });
+
+/**
+ * 循环移动
+ */
+function gameloop() {
+	movePaddles();
+}
+
+function movePaddles() {
+	var bh = parseInt($("#playground").css("height"));
+
+	if (pingpong.pressedKeys[KEY.UP]) {
+		var top = parseInt($("#paddleB").css("top"));
+		if (checkTopBound(top)) {
+			$("#paddleB").css("top", top - 5);
+		}
+	}
+	if (pingpong.pressedKeys[KEY.DOWN]) {
+		var top = parseInt($("#paddleB").css("top"));
+		var oh = parseInt($("#paddleB").css("height"));
+		if (checkBottomBound(bh, oh, top)) {
+			$("#paddleB").css("top", top + 5);
+		}
+	}
+	if (pingpong.pressedKeys[KEY.W]) {
+		var top = parseInt($("#paddleA").css("top"));
+		if (checkTopBound(top)) {
+			$("#paddleA").css("top", top - 5);
+		}
+	}
+	if (pingpong.pressedKeys[KEY.S]) {
+		var top = parseInt($("#paddleA").css("top"));
+		var oh = parseInt($("#paddleA").css("height"));
+		if (checkBottomBound(bh, oh, top)) {
+			$("#paddleA").css("top", top + 5);
+		}
+	}
+}
+
+/**
+ * 游戏区域上边界检验
+ */
+function checkTopBound(objtop) {
+	return objtop > 0;
+}
+/**
+ * 游戏区域下边界检验
+ */
+function checkBottomBound(backheight, objheight, objtop) {
+	return backheight > objheight + objtop;
+}
